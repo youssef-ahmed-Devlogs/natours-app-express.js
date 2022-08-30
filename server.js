7f2a9cf3959556e56797793b134b6bc331030783
youssef-ahmed-Devlogs <youssefahmedsup@gmail.com>
2022-08-30 23:25:13 +0200

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
-
+//
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
