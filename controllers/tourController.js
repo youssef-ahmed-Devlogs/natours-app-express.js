503835cedd3e3b9d8158514071e4405e48900369
youssef-ahmed-Devlogs <youssefahmedsup@gmail.com>
2022-09-04 22:39:21 +0200

    Updates

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -52,7 +52,6 @@ exports.getTour = async (req, res) => {
 exports.createTour = async (req, res) => {
   try {
     const newTour = await Tour.create(req.body);
-
     res.status(201).json({
       status: 'success',
       data: {
