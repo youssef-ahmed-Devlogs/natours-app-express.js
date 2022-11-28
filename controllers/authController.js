a72a39f38611448b1230b396e9a82565dbc72ff2
youssef-ahmed-Devlogs <youssefahmedsup@gmail.com>
2022-11-28 05:11:55 +0200

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const Email = require('./../utils/email'); //
+const Email = require('./../utils/email');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
